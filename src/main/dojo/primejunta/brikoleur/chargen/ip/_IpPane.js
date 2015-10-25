8ee4908bba9b5d225fdb431ae71029da01b07166
psulonen <petteri.sulonen@iki.fi>
2015-10-25 11:15:05 +0100

    Adds rest of in-play panes to UI framework.

--- src/main/dojo/primejunta/brikoleur/chargen/ip/_IpPane.js
+++ src/main/dojo/primejunta/brikoleur/chargen/ip/_IpPane.js
@@ -3,12 +3,22 @@ define( [ "dojo/_base/declare",
           "../_base/_MainPaneBase",
           "./name/NamePane",
           "./resolver/ResolverPane",
+          "./inventory/InventoryPane",
+          "./numbers/NumbersPane",
+          "./powers/PowersPane",
+          "./stunts/StuntsPane",
+          "./ohun/OhunPane",
           "dojo/i18n!primejunta/brikoleur/nls/CharGen" ],
 function( declare,
           lang,
           _MainPaneBase,
           NamePane,
           ResolverPane,
+          InventoryPane,
+          NumbersPane,
+          PowersPane,
+          StuntsPane,
+          OhunPane,
           i18n )
 {
     return declare( [ _MainPaneBase ], {
@@ -22,6 +32,11 @@ function( declare,
         {
             this._addPane( "name", new NamePane().placeAt( this.nameContainer ) );
             this._addPane( "resolver", new ResolverPane( { dock : this.dockContainer } ).placeAt( this.dynamicGrid ) );
+            this._addPane( "numbers", new NumbersPane( { dock : this.dockContainer } ).placeAt( this.dynamicGrid ) );
+            this._addPane( "powers", new PowersPane( { dock : this.dockContainer } ).placeAt( this.dynamicGrid ) );
+            this._addPane( "ohun", new OhunPane( { dock : this.dockContainer } ).placeAt( this.dynamicGrid ) );
+            this._addPane( "stunts", new StuntsPane( { dock : this.dockContainer } ).placeAt( this.dynamicGrid ) );
+            this._addPane( "gear", new InventoryPane( { dock : this.dockContainer } ).placeAt( this.dynamicGrid ) );
         }
     } );
 } );
---
+++ src/main/dojo/primejunta/brikoleur/chargen/ip/inventory/InventoryPane.js
@@ -0,0 +1,34 @@
+/**
+ * Task resolver pane.
+ *
+ * @public Widget
+ */
+define([ "dojo/_base/declare",
+         "dojo/_base/lang",
+         "dojo/topic",
+         "./../../_base/_FeaturePaneBase",
+         "dojo/i18n!primejunta/brikoleur/nls/CharGen" ],
+function( declare,
+          lang,
+          topic,
+          _FeaturePaneBase,
+          i18n )
+{
+    return declare( [ _FeaturePaneBase ],
+    {
+        /**
+         * Title.
+         *
+         * @final
+         * @public string
+         */
+        title : i18n.Gear,
+        /**
+         * Icon.
+         *
+         * @final
+         * @public string
+         */
+        icon : "suitcase"
+    } );
+});
